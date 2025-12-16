f9d4fc31464c3a6f50fb597edc2cd8e86ff6bf74
KornilovaEV <61602070+KornilovaEV@users.noreply.github.com>
2025-12-16 19:30:20 +0400

    Delete .history directory

--- .history/src/index_20251216153417.js
+++
@@ -1,8 +0,0 @@
-import { Application, Assets, Sprite, Container } from "pixi.js";
-
-(async () => {
-  const app = new Application();
-  await app.init({ background: "#545454", }); //  resizeTo: window 
-  document.body.appendChild(app.canvas);
-
-})();
--- .history/src/index_20251216153809.js
+++
@@ -1,6 +0,0 @@
-(async () => {
-  const app = new Application();
-  await app.init({ background: "#545454", }); //  resizeTo: window 
-  document.body.appendChild(app.canvas);
-
-})();
